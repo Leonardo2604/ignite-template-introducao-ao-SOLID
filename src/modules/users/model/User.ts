82bdd5cfbc7e89845103b21482f1653afcaa6bd2
Leonardo Rangel dos Santos <leonardorangel2604@gmail.com>
2021-10-16 19:17:22 -0300

    feat: add new features

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,27 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  public admin: boolean;
+  public id: string;
+  public created_at: Date;
+  public updated_at: Date;
+  public name: string;
+  public email: string;
+
+  constructor(
+    name: string,
+    email: string,
+    id?: string,
+    created_at?: Date,
+    updated_at?: Date
+  ) {
+    this.id = id || uuidV4();
+    this.name = name;
+    this.email = email;
+    this.admin = false;
+    this.created_at = created_at || new Date();
+    this.updated_at = updated_at || new Date();
+  }
 }
 
 export { User };
